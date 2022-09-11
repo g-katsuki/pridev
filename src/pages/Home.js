4cb7ec610c97efb80e17c4c6326ed98b95275e92
Go Katsuki <go.katsuki@rakuten.com>
2022-09-11 20:57:14 +0900

    success output

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+// import { ApiFetch } from "./ApiFetch";
+
+const Home = () => {
+  return (
+    <>
+      <h1>ホーム</h1>
+      <div>
+        {/* <Link to={`/page1/`}>こちら</Link> */}
+      </div>
+      {/* <ApiFetch /> */}
+    </>
+  );
+};
+
+export default Home;
