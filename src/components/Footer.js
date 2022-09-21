a03597a3d79eb3feeab349e22d1aab3134f5d000
Go Katsuki <go.katsuki@rakuten.com>
2022-09-21 21:13:19 +0900

    add footer commented out

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div class="fixed-bottom mx-0 bg-primary bg-opacity-75 text-center text-white">
+    <h5>@BFT なごやしてん</h5>
+  </div>
+  );
+};
+
+export default Footer;
