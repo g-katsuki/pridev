1757b250202518f2254a9916deeb377da9aa1a2a
Go Katsuki <go.katsuki@rakuten.com>
2022-09-26 09:20:13 +0900

    add 404.html

--- src/pages/Calc.js
+++ src/pages/Calc.js
@@ -4,9 +4,6 @@ import { Calculate } from '../components/Calculate'
 function Calc() {
   return (
     <div>
-      {/* <li>ここではフレームの計算を行う</li>
-      <li>攻撃ファイターと攻撃名、ガードファイターを選択</li>
-      <li>確定反撃のとれる技一覧を表示</li> */}
       <Calculate />
     </div>
   )
